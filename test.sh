e5cce15fe86fcb3e9e15062c8f6a1f4f1a856287
Sejin Kim <40668167+skim2257@users.noreply.github.com>
2021-04-16 15:10:03 -0400

    push to origin

---
+++ test.sh
@@ -0,0 +1,1 @@
+python train.py --demo --demo_input_path dataset/data/ShapeNetV1PointCloud/02691156/10155655850468db78d106ce0a280f87.points.ply.npy --reload_model_path log/02021-04-03T16\:25\:08.091652/network.pth
